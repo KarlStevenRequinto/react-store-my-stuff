f3187790c7dd251aa5828047b7f1b7e0dd986268
KarlStevenRequinto <lerdrobott@gmail.com>
2025-04-15 17:24:13 +0800

    initialize project to github repo

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+export const metadata: Metadata = {
+  title: "StoreIt",
+  description: "StoreIt - The only storage solution you need.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>Layouy</div>
+  );
+}
